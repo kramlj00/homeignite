1090232cc67780ebb16926b1bd4f82d14a266d31
kramlj00 <kristinaramljak5@gmail.com>
2025-01-18 17:55:05 +0100

    Added hero image

--- src/features/hero/hero.tsx
+++ src/features/hero/hero.tsx
@@ -1,6 +1,17 @@
+import Image from "next/image";
+import HeroImage from "@/assets/images/hero-image.png";
+
 const Hero = () => {
   return (
-    <div className="bg-black min-h-screen bg-hero-background bg-cover bg-center"></div>
+    <div className="relative bg-black min-h-screen bg-hero-background bg-cover bg-center w-full">
+      <div className="absolute bottom-0 right-[170px]">
+        <div className="relative bg-white/5 px-28 pt-4">
+          <div className="relative [mask-image:linear-gradient(to_bottom,white_50%,transparent_100%)]">
+            <Image src={HeroImage} alt="Hero" />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
