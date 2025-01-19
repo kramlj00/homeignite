3e9df419282e36a96e4632aae3e29da6ea8b2290
kramlj00 <kristinaramljak5@gmail.com>
2025-01-19 11:40:47 +0100

    Updated responsivenes

--- src/features/hero/hero-actions.tsx
+++ src/features/hero/hero-actions.tsx
@@ -5,11 +5,11 @@ import Button from "@/components/button";
 const HeroActions = () => {
   return (
     <div className="flex items-center gap-4 sm:gap-10 w-full justify-center absolute bottom-12">
-      <p className="sm:block hidden text-white text-xs sm:text-sm font-semibold uppercase">
+      <p className="sm:block hidden text-white text-xs sm:text-sm font-semibold uppercase cursor-pointer">
         See pricing
       </p>
       <Button variant={BUTTON_VARIANT.PRIMARY}>Contact us</Button>
-      <p className="sm:block hidden text-white text-xs sm:text-sm font-semibold uppercase">
+      <p className="sm:block hidden text-white text-xs sm:text-sm font-semibold uppercase cursor-pointer">
         Request a quote
       </p>
     </div>
--- src/features/hero/hero.tsx
+++ src/features/hero/hero.tsx
@@ -8,7 +8,7 @@ import HeroActions from "./hero-actions";
 const Hero = () => {
   return (
     <div className="relative bg-black min-h-screen bg-hero-background bg-cover bg-center w-full">
-      <div className="absolute top-80 right-[500px]">
+      <div className="absolute bottom-80 right-[500px]">
         <RectangleCard className="z-50 m-auto">
           <div className="flex flex-col gap-1">
             <Image src={RectangleImage1} alt="Hero" />
@@ -19,7 +19,7 @@ const Hero = () => {
         </RectangleCard>
       </div>
 
-      <div className="absolute top-96 right-[20px]">
+      <div className="absolute bottom-44 right-8">
         <RectangleCard className="z-50">
           <div className="flex flex-col gap-1">
             <Image src={RectangleImage2} alt="Hero" />
@@ -30,10 +30,14 @@ const Hero = () => {
         </RectangleCard>
       </div>
 
-      <div className="absolute top-32 right-[60px]">
-        <div className="relative bg-white/5 px-28 pt-4">
+      <div className="absolute top-28 md:top-auto md:bottom-0 md:right-[60px] left-1/2 md:left-auto -translate-x-1/2 md:translate-x-0 w-[70vw] md:w-fit">
+        <div className="relative px-10 md:px-28 pt-4 bg-gradient-to-b from-white/5 to-transparent">
           <div className="relative [mask-image:linear-gradient(to_bottom,white_50%,transparent_100%)]">
-            <Image src={HeroImage} alt="Hero" />
+            <Image
+              src={HeroImage}
+              alt="Hero"
+              className="h-[45vh] md:h-[calc(100vh-140px)] object-cover"
+            />
           </div>
         </div>
       </div>
