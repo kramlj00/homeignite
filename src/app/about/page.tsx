3c77316b8d9bf647498e75363f2f16713915c2a0
kramlj00 <kristinaramljak5@gmail.com>
2025-02-16 16:01:51 +0100

    Added about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,30 @@
+import { Metadata } from "next";
+import { ResolvingMetadata } from "next";
+
+import { getMetadataTitle } from "@/lib/metadata/metadata-utils";
+import { openGraphImage } from "@/lib/metadata/open-graph-image";
+import { ABOUT_ROUTE } from "@/constants";
+
+export async function generateMetadata(
+  _arg: Record<string, never>,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const parentOpengraphUrl = (await parent).openGraph?.url;
+
+  return {
+    title: getMetadataTitle("About"),
+    openGraph: {
+      title: getMetadataTitle("About"),
+      url: `${parentOpengraphUrl}${ABOUT_ROUTE}`,
+      ...openGraphImage,
+    },
+  };
+}
+
+const AboutPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen"></div>
+  );
+};
+
+export default AboutPage;
