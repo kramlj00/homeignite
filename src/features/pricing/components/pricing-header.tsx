be54bf30b3adbad7a8b02906f4b2c0f9226cf5f0
kramlj00 <kristinaramljak5@gmail.com>
2025-02-21 15:50:47 +0100

    Added pricing page

---
+++ src/features/pricing/components/pricing-header.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import PricingHeaderImage from "@/assets/images/pricing-header.png";
+
+const PricingHeader = () => {
+  return (
+    <section className="relative min-h-screen">
+      <div className="absolute inset-0 w-full">
+        <Image
+          src={PricingHeaderImage}
+          alt="Modern interior living space with wooden furniture and minimalist design"
+          fill
+          priority
+          className="object-cover w-full"
+        />
+      </div>
+
+      <div className="relative mt-[140px] ml-[140px] flex flex-col gap-10 text-white z-10 min-h-screen">
+        <h1 className="text-4xl font-gotham-medium">Find Your Perfect Plan</h1>
+        <p className="text-lg font-gotham-medium">
+          Clearly state the value proposition, e.g., &quot;Affordable Plans for
+          <br />
+          Every Need.&quot;
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default PricingHeader;
