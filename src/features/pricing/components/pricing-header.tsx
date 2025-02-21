1f843a28a24b0f1145bcb35c5d1873520b1c60cb
kramlj00 <kristinaramljak5@gmail.com>
2025-02-21 16:49:37 +0100

    Added pricing plans

--- src/features/pricing/components/pricing-header.tsx
+++ src/features/pricing/components/pricing-header.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import PricingHeaderImage from "@/assets/images/pricing-header.png";
-
+import PricingPlans from "./pricing-plans";
 const PricingHeader = () => {
   return (
     <section className="relative min-h-screen">
@@ -14,13 +14,16 @@ const PricingHeader = () => {
         />
       </div>
 
-      <div className="relative mt-[140px] ml-[140px] flex flex-col gap-10 text-white z-10 min-h-screen">
+      <div className="relative mt-[140px] ml-[140px] flex flex-col gap-10 text-white z-10">
         <h1 className="text-4xl font-gotham-medium">Find Your Perfect Plan</h1>
         <p className="text-lg font-gotham-medium">
           Clearly state the value proposition, e.g., &quot;Affordable Plans for
           <br />
           Every Need.&quot;
         </p>
+      </div>
+      <div className="relative">
+        <PricingPlans />
       </div>
     </section>
   );
